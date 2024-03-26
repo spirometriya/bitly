2e37451ecae12f8a415e255943190aecaf108bbf
Alexey <alexey@alexey>
2024-03-26 10:11:10 +0300

    #

--- main.py
+++ main.py
@@ -15,33 +15,33 @@ def shorten_link(token, url):
 
 
 def count_clicks(token, link):
-    parse_url = urlparse(link)
+    parsing_url = urlparse(link)
     headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(f"{API_URL}{parse_url.netloc}{parse_url.path}/clicks/summary", headers=headers)
+    response = requests.get(f"{API_URL}{parsing_url.netloc}{parsing_url.path}/clicks/summary", headers=headers)
     response.raise_for_status()
     return response.json().get("total_clicks")
 
 
 def is_bitlink(token, url):
-    parse_url = urlparse(url)
+    parsing_url = urlparse(url)
     headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(f"{API_URL}{parse_url.netloc}{parse_url.path}", headers=headers)
-    if response.ok:
-        return True
+    response = requests.get(f"{API_URL}{parsing_url.netloc}{parsing_url.path}", headers=headers)
+    return response.ok
 
 
 if __name__ == '__main__':
     load_dotenv()
     token = os.environ["BITLY_BEARER_TOKEN"]
     url = input()
-    if not is_bitlink(token, url):
+    if is_bitlink(token, url):
+        try:
+            click_count = count_clicks(token, url)
+            print(f"Your url was clicked {click_count} time(s)")
+        except requests.HTTPError:
+            print(f"Could not get the number of clicks for {url}")
+    else:
         try:
             url = shorten_link(token, url)
             print(f"Short url: {url}")
         except requests.HTTPError:
             print("Invalid url for shortening!")
-    try:
-        click_count = count_clicks(token, url)
-        print(f"Your url was clicked {click_count} time(s)")
-    except requests.HTTPError:
-        print(f"Could not get the number of clicks for {url}")
